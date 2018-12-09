f34042c2a3058660b01fe9e714b1ee016180f816
Frederico <frederico.lopes.goncalves@gmail.com>
2018-12-09 17:45:50 +0100

    Adds error animation

--- lib/presentation/widgets/ColorItem.dart
+++ lib/presentation/widgets/ColorItem.dart
@@ -3,6 +3,8 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
+enum ColorItemState { ready, inflating, shaking }
+
 class ColorItem extends StatefulWidget {
   final int color;
   final double radius;
@@ -21,42 +23,74 @@ class ColorItem extends StatefulWidget {
   _ColorItemState createState() => _ColorItemState();
 }
 
-class _ColorItemState extends State<ColorItem>
-    with SingleTickerProviderStateMixin {
-  AnimationController _controller;
+class _ColorItemState extends State<ColorItem> with TickerProviderStateMixin {
+  AnimationController _inflateAnimationController;
+  AnimationController _sizeAnimationController;
+  ColorItemState _itemState;
+  Random _random;
 
   @override
   void initState() {
-    _controller = AnimationController(
+    _itemState = ColorItemState.inflating;
+    _random = Random();
+    _sizeAnimationController = _getSizeAnimationController();
+
+    _inflateAnimationController = AnimationController(
         vsync: this,
         duration: Duration(
           milliseconds: 300,
         ));
 
-    _controller.addListener(() {
-      if (_controller.value == 1) {
+    _inflateAnimationController.addListener(() {
+      if (_inflateAnimationController.value == 1) {
         widget.onAnimationFinished();
+        setState(() => _itemState = ColorItemState.ready);
         return;
       }
 
       setState(() {});
     });
 
-    var timer = (500 * Random().nextDouble()).toInt();
+    var timer = (500 * _random.nextDouble()).toInt();
 
-    Timer(Duration(milliseconds: timer), () => _controller.forward());
+    Timer(Duration(milliseconds: timer),
+        () => _inflateAnimationController.forward());
 
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: widget.onTap,
+    return GestureDetector(
+      onTap: () {
+        if (_itemState == ColorItemState.ready) {
+          _sizeAnimationController.forward(from: 0.0);
+          widget.onTap();
+        }
+      },
       child: CircleAvatar(
-        radius: widget.radius * _controller.value,
+        radius: widget.radius * _inflateAnimationController.value -
+            _sizeAnimationController.value *
+                80 *
+                sin(4 * pi * _sizeAnimationController.value),
         backgroundColor: Color(widget.color),
       ),
     );
+  }
+
+  AnimationController _getSizeAnimationController() {
+    AnimationController controller = AnimationController(
+        vsync: this, duration: new Duration(milliseconds: 300));
+
+    controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        controller.reverse();
+      }
+    });
+
+    controller.addListener(() {
+      setState(() {});
+    });
+    return controller;
   }
 }
